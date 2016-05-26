6194ec54ee36cfd0274c360d776b9abac5602918
Doug Latornell <doug.latornell@43ravens.ca>
2016-05-25 19:03:35 -0700

    Initialize NEMO_Nowcast Python package.

---
+++ __pkg_metadata__.py
@@ -0,0 +1,20 @@
+# Copyright 2016 Doug Latornell, 43ravens
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""NEMO_Nowcast -- NEMO ocean model nowcast framework
+"""
+PROJECT = 'NEMO_Nowcast'
+DESCRIPTION = 'NEMO ocean model nowcast framework'
+VERSION = '0.1'
+DEV_STATUS = '2 - Pre-Alpha'
