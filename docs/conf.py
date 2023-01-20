263bed4dcfe1af37060dbbba69f7e301b9bfd328
Doug Latornell <doug.latornell@43ravens.ca>
2023-01-20 15:27:50 -0800

    Drop static path list in Sphinx config
    To silence warning `_static` path does not exist.
    The docs source files don't include any static content.

--- docs/conf.py
+++ docs/conf.py
@@ -91,11 +91,6 @@ html_theme = "sphinx_rtd_theme"
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title = "NEMO Nowcast"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 html_last_updated_fmt = "%b %d, %Y"
