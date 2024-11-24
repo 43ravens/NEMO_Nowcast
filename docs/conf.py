43ravens/NEMO_Nowcast

commit bdfcba5a126223605ee158ce4a589dbbe5730c37

Change to use importlib.metadata.version()
More robust, consistent, and modern.

docs/conf.py
@@ -13,7 +13,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 
-import datetime
+import importlib.metadata
 import os
 import sys
 
@@ -30,9 +30,7 @@ copyright_years = (
 copyright = f"{copyright_years}, {author}"
 
 # The short X.Y version.
-from nemo_nowcast import __pkg_metadata__
-
-version = __pkg_metadata__.VERSION
+version = importlib.metadata.version()
 # The full version, including alpha/beta/rc tags.
 release = version
 

commit d473af9e8e80f6253555de4ba04058fd907e858f

Correct fetching of package version in Sphinx config
Fixes bug introduced in bdfcba5a12622.

docs/conf.py
@@ -22,14 +22,14 @@ sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
-project = "NEMO Nowcast Framework"
+distribution_name = "NEMO_Nowcast"
 author = "Doug Latornell"
 pkg_creation_year = 2016
 copyright_years = f"{pkg_creation_year} – present"
 copyright = f"{copyright_years}, {author}"
 
 # The short X.Y version.
-version = importlib.metadata.version()
+version = importlib.metadata.version(distribution_name)
 # The full version, including alpha/beta/rc tags.
 release = version
 